11ec9054769d0d978fa59c9e486536ff020b53db
rashidmrv <147900070+rashidmrv@users.noreply.github.com>
2023-12-22 07:06:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,3 +75,21 @@ $(document).ready(function() {
 })
 
 //  gallery-section - owl-carousel
+
+
+// Detect request animation frame
+let observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden')
+
+hiddenElement.forEach((el) => observer.observe(el))
